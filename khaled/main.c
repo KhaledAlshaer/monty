52e8a06c1c103d8a89ec7877b1e00c4f08a5c16b
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2023-11-16 10:06:16 -0800

    test

---
+++ khaled/main.c
@@ -0,0 +1,48 @@
+#include "monty.h"
+
+/**
+ * main- the entry point
+ * @argc: the arguments count
+ * @argv: the arguments vector
+ * Return: 0 successful
+*/
+
+int  main(int argc, char *argv[])
+{
+	FILE *file;
+	char line[256];
+	unsigned int line_number = 1;
+	stack_t *head = NULL;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		file = fopen(argv[1], "r");
+		if (file == NULL)
+		{
+			fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			while (fgets(line, sizeof(line), file) != NULL)
+			{
+				trim(line);
+				if (line_proccessing(&head, line, line_number) == 0)
+				{
+					destroy_stack(&head);
+					fclose(file);
+					exit(EXIT_FAILURE);
+				}
+				line_number++;
+			}
+		}
+		destroy_stack(&head);
+		fclose(file);
+	}
+	return (0);
+}
---
+++ khaled/opcodes1.c
@@ -0,0 +1,94 @@
+#include "monty.h"
+
+/**
+ * push- pushing nodes
+ * @stack: the stack header
+ * @line_number: the current line index
+*/
+
+void push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new;
+	char *token = strtok(NULL, " \t\n");
+	int val;
+
+	if (token == NULL || *token == '\n' || *token < '0' || *token > '9')
+	{
+		destroy_stack(stack);
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	val = atoi(token);
+
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		destroy_stack(stack);
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	new->n = val;
+	new->prev = NULL;
+	new->next = *stack;
+
+	if (*stack != NULL)
+		(*stack)->prev = new;
+
+	*stack = new;
+}
+
+/**
+ * pall- printing the stack
+ * @stack: the stack header
+ * @line_number: the current line index
+*/
+
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *ptr;
+	(void)line_number;
+	ptr = *stack;
+
+	while (ptr)
+	{
+		fprintf(stdout, "%d\n", ptr->n);
+		ptr = ptr->next;
+	}
+}
+
+/**
+ * pint- print the heading node value
+ * @stack: the stack header
+ * @line_number: the current line index
+*/
+
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		destroy_stack(stack);
+		fprintf(stderr, "L%u>: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	fprintf(stdout, "%d\n", (*stack)->n);
+}
+
+/**
+ * destroy_stack- freeing the stack
+ * @stack: the stack header
+*/
+
+void destroy_stack(stack_t **stack)
+{
+	stack_t *temp;
+
+	while (*stack)
+	{
+		temp = *stack;
+		*stack = (*stack)->next;
+		free(temp);
+	}
+}
