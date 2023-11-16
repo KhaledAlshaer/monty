2b5eafe3ffa7b80ddaad107aaf3ec5adea1f13d9
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2023-11-16 11:00:57 -0800

    nop

--- monty.h
+++ monty.h
@@ -48,6 +48,7 @@ void destroy_stack(stack_t **stack);
 
 void trim(char *str);
 void nop(stack_t **stack, unsigned int line_number);
+int is_valid_num(char *token);
 int line_proccessing(stack_t **stack, char *line, unsigned int line_index);
 
 #endif
